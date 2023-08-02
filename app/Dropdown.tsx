43ea0d676bce91101b4bab605ba97ccf62e80fa3
Elisha Nam <namelisha@gmail.com>
2023-08-02 00:46:52 -0700

    Added rotation animation to menu

--- app/Dropdown.tsx
+++ app/Dropdown.tsx
@@ -1,13 +1,35 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 function Dropdown() {
+  const [mouseOver, setMouseOver] = useState(false);
+
   return (
-    <ul className="text-[#49c5b6] fixed top-4 left-8">
+    <ul
+      className="text-[#49c5b6] fixed top-4 left-8"
+      onMouseEnter={() => setMouseOver(true)}
+      onMouseLeave={() => setMouseOver(false)}
+    >
       <li>
-        <h1 className="text-md hover:text-3xl duration-500">Menu</h1>
-        <ul>
+        <Image
+          className={
+            "text-md overflow-hidden change-color transition-all duration-300 " +
+            (mouseOver ? "-rotate-90" : "")
+          }
+          src="/../public/menu-flat.png"
+          width="32"
+          height="32"
+          alt="Menu"
+        ></Image>
+        <ul
+          className={
+            "absolute transition-all duration-500 " +
+            (mouseOver ? "" : "text-black")
+          }
+        >
           <li>
             <Link href="/">Home</Link>
           </li>

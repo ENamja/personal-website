d4a6d2671d34bb20c4d0ea4535e7c3ac3d7c1f57
Elisha Nam <namelisha@gmail.com>
2023-08-04 16:09:28 -0700

    Started typing animation

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,10 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="text-[#49c5b6] text-center font-light">
-      <h1 className="text-9xl my-12 pt-16">Hello</h1>
+      <h1 className="text-9xl my-12 pt-16 inline-block overflow-hidden whitespace-nowrap animate-typing">
+        &lt;Hello/&gt;
+        <span className="box-border inline-block w-1 h-32 ml-2 -mb-4 bg-white animate-cursor"></span>
+      </h1>
       <div className="text-6xl">
         <p className="text-left my-6">
           &lt; I'm <span className="text-white font-normal">Elisha Nam</span>,
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        cursor: 'cursor .6s linear infinite alternate',
+        typing: 'typing 1.8s ease-out .8s 1 normal both',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -20,6 +24,22 @@ module.exports = {
           '0%' : {opacity: '0', transform: 'rotateX(-90deg)'},
           '50%': {transform: 'rotateX(-20deg)'},
           '100': {opacity: '1', transform: 'rotateX(0deg)'}
+        },
+        typing: {
+          '0%': { width: '0ch' },
+          '10%, 15%': { width: '1ch' },
+          '20%, 25%': { width: '2ch' },
+          '30%, 35%': { width: '3ch' },
+          '40%, 45%': { width: '4ch' },
+          '50%, 55%': { width: '5ch' },
+          '60%, 65%': { width: '6ch' },
+          '70%, 75%': { width: '7ch' },
+          '80%, 85%': { width: '8ch' },
+          '90%, 95%': { width: '9ch' },
+        },
+        cursor: {
+          '0%, 40%': {opacity: '1'},
+          '60%, 100%': {opacity: '0'},
         }
       },
       fontFamily: {

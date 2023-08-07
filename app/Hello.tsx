6789199c1fa200e09dce998e93ab054a13a93e70
Elisha Nam <namelisha@gmail.com>
2023-08-07 11:15:35 -0700

    Removed useState

--- app/Hello.tsx
+++ app/Hello.tsx
@@ -1,32 +1,13 @@
 "use client";
 
-import { useState } from "react";
-
 function Hello() {
-  // true => dark; false => light
-  const [theme, setTheme] = useState(true);
-
   return (
-    <div
-      className={
-        "flex justify-center items-center text-center h-screen" +
-        (theme ? " bg-black" : " bg-white")
-      }
-    >
-      <div
-        className={
-          "font-light inline-block text-9xl" + (theme ? " text-[#49c5b6]" : "")
-        }
-      >
+    <div className="flex justify-center items-center text-center h-screen bg-black">
+      <div className="font-light inline-block text-9xl text-[#49c5b6]">
         <span className="inline-block overflow-hidden animate-typing">
           &lt;Hello/&gt;
         </span>
-        <span
-          className={
-            "box-border inline-block w-1 h-32 ml-2 -mb-4 animate-cursor" +
-            (theme ? " bg-white" : "")
-          }
-        ></span>
+        <span className="box-border inline-block w-1 h-32 ml-2 -mb-4 animate-cursor bg-white"></span>
       </div>
     </div>
   );

a3ef43a956dcfce397f20d0a4fd37ecd507e5f87
Elisha Nam <namelisha@gmail.com>
2023-08-08 12:47:31 -0700

    Added and styled theme toggle

--- app/Toggle.tsx
+++ app/Toggle.tsx
@@ -7,7 +7,7 @@ interface ToggleProps {
   setTheme: Dispatch<SetStateAction<string>>;
 }
 
-function handleClick({ theme, setTheme }: ToggleProps) {
+function handleChange({ theme, setTheme }: ToggleProps) {
   theme == "dark" ? setTheme("light") : setTheme("dark");
 }
 
@@ -19,7 +19,22 @@ function Toggle({ theme, setTheme }: ToggleProps) {
         (theme === "dark" ? " text-white" : " text-black")
       }
     >
-      <button onClick={() => handleClick({ theme, setTheme })}>Theme</button>
+      <input
+        className="w-0 h-0 hidden"
+        type="checkbox"
+        id="toggle"
+        onChange={() => handleChange({ theme, setTheme })}
+      />
+      <label
+        className={
+          "duration-500 w-14 h-8 relative block rounded-2xl cursor-pointer after:content-[''] after:w-6 after:h-6 after:absolute \
+          after:top-1 after:left-1 after:bg-gradient-to-b after:rounded-[12px] after:duration-500 after:transition-all" +
+          (theme === "dark"
+            ? " shadow-[inset_0_-1.5px_3px_rgba(255,255,255,0.4)] after:from-[#777] after:to-[#3a3a3a] bg-[#242424]"
+            : " shadow-[inset_0_1.5px_3px_rgba(0,0,0,0.4)] after:from-[#ffffff] after:to-[#e0e0e0] after:translate-x-[100%] bg-[#d4d4d4]")
+        }
+        htmlFor="toggle"
+      ></label>
     </div>
   );
 }

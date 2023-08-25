843964a9c32d04614bd2c4f867c1b08a20023ae1
Elisha Nam <namelisha@gmail.com>
2023-08-25 16:52:54 -0700

    Start Project Page

--- app/projects/ProjectList.tsx
+++ app/projects/ProjectList.tsx
@@ -5,10 +5,22 @@ import { ThemeContext } from "@/app/Contexts";
 
 function ProjectList() {
   const theme = useContext(ThemeContext);
+  const projects = [
+    { name: "Personal Website", link: "localhost:3000" },
+    { name: "Minesweeper Solver", link: "localhost:3000" },
+    { name: "Apollo", link: "localhost:3000" },
+    { name: "Filler", link: "localhost:3000" },
+  ];
 
   return (
-    <div>
-      <div></div>
+    <div className="flex flex-row">
+      {projects.map((project) => {
+        return (
+          <div id={project.name} className="w-[1000]">
+            {project.name} {project.link}
+          </div>
+        );
+      })}
     </div>
   );
 }

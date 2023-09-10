86be2fac798865b26d4459da3e38c54faf382c83
Elisha Nam <namelisha@gmail.com>
2023-09-10 16:53:43 -0700

    Spaced text in project boxes

--- app/projects/ProjectList.tsx
+++ app/projects/ProjectList.tsx
@@ -9,9 +9,9 @@ function ProjectList() {
   const projects = [
     {
       name: "Personal Website",
-      link: "localhost:3000",
+      link: "https://github.com/ENamja/personal-website",
       description:
-        "This personal website was built as a dynamic extension of my resume, offering a comprehensive overview of my qualifications as a student developer. The site showcases my downloadable up-to-date resume for easy reference, key projects, and skillsets.",
+        "The website you are on was built as a dynamic extension of my resume, offering a comprehensive overview of my qualifications as a student developer. The site showcases my downloadable up-to-date resume for easy reference, key projects, and skillsets.",
       tools: ["Next.js", "Node.js", "TailwindCSS", "Typescript", "Vercel"],
     },
     {
@@ -23,7 +23,7 @@ function ProjectList() {
     },
     {
       name: "Apollo",
-      link: "localhost:3000",
+      link: "https://github.com/lanpai/apollo",
       description:
         "This is a Unity-based educational chemistry game that uses the concept of crafting to teach basic chemical compounds. The game focuses on a spaceman, Apollo, who must deconstruct items to their basic elements to create other needed items to repair his failing spaceship. Through our team's overall problem-solving skills, teamwork, and excellent visuals, our game was awarded first place in HackSoCal 2020.",
       tools: ["C#", "Unity"],
@@ -42,20 +42,22 @@ function ProjectList() {
           >
             <div
               className={
-                "transition-all w-full duration-500 p-4 my-4 rounded-3xl drop-shadow-lg" +
+                "transition-all w-full duration-500 p-8 my-4 rounded-3xl drop-shadow-lg" +
                 (theme === "dark" ? " text-white" : " bg-white text-black")
               }
             >
-              <div className="absolute rounded-3xl top-0 left-0 w-full h-full bg-white opacity-[.10]"></div>
               <Link
                 href={project.link}
                 target="_blank"
-                className="w-full h-full"
-              >
-                <div className="text-4xl">{project.name}</div>
-                <div className="text-2xl">{project.description}</div>
-                <div className="text-2xl">{project.tools}</div>
-              </Link>
+                className="absolute rounded-3xl top-0 left-0 w-full h-full bg-white opacity-[.10]"
+              ></Link>
+              <div className="w-full h-full text-left">
+                <div className="mb-4 text-3xl">{project.name}</div>
+                <div className="my-8 font-light text-xl">
+                  {project.description}
+                </div>
+                <div className="font-light text-xl">{project.tools}</div>
+              </div>
             </div>
           </li>
         );

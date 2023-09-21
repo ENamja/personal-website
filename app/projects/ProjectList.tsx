69d7f60c8982d093128eed3948cfa83cdff3146a
Elisha Nam <namelisha@gmail.com>
2023-09-20 17:50:11 -0700

    Added to Minesweeper Solver description

--- app/projects/ProjectList.tsx
+++ app/projects/ProjectList.tsx
@@ -26,7 +26,7 @@ function ProjectList() {
       name: "Minesweeper Solver",
       link: "localhost:3000",
       description:
-        "This is a Minesweeper-solving algorithm that demonstrates my problem-solving and programming skills. ",
+        "This is a Minesweeper-solving algorithm that demonstrates my problem-solving and programming skills. The algorithm was a class project for an AI class with the goal of accurately and efficiently solving Minesweeper worlds of varying sizes. Our solution was built through the application of various ideas such as some basic fundamentals of AI like heuristic search, a search algorithm, and even Harvard sentence-reduction logic.",
       tools: ["Python"],
     },
     {
